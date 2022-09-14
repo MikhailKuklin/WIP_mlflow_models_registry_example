80e5583a0e3693e2e2b8edff746fcb505c176f92
Mikhail Kuklin <kuklin1990@gmail.com>
2022-09-14 21:46:11 +0300

    update

--- web-service-flask/test.py
+++ web-service-flask/test.py
@@ -4,18 +4,19 @@ import numpy as np
 import json
 import predict
 
-features = np.load("features.npy")
+# features = np.load("features.npy")
 
-# features = {"PULocationID": 10, "DOLocationID": 50, "trip_distance": 40}
+# features = {"F": features}
 
 # features_json = json.dumps({"features": features.tolist()})
 
 # features_json = json.dumps({features.tolist()})
 
-# url = "http://localhost:6000/predict"
+url = "http://localhost:6000/predict"
 
-# response = requests.post(url, json=features_json)
-# print(response.json())
+# response = requests.post(url, json=features)
+response = requests.post(url, json=features)
+print(response.json())
 
-pred = predict.predict(features)
-print(pred)
+# pred = predict.predict(features)
+# print(pred)
