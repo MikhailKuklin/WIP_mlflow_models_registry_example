2147fd4aeb0612a6f810dd242a68b6a051c2ba38
Mikhail Kuklin <kuklin1990@gmail.com>
2022-09-15 21:09:35 +0300

    update

--- web-service-flask/predict.py
+++ web-service-flask/predict.py
@@ -7,10 +7,10 @@ with open("model_log_reg.bin", "rb") as f_in:
     model = pickle.load(f_in)
 
 
-
-
 def predict(features):
-    X = np.array(features[['f1','f2','f3','f4','f5','f6', 'f7','f8','f9','f10','f11'']])
+    X = np.array(
+        features[["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11"]]
+    )
     preds = model.predict(X)
     return preds
 
