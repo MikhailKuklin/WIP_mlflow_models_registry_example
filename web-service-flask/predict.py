22d37a5d500d18110fd37c4126471d8a5b7a5453
Mikhail Kuklin <kuklin1990@gmail.com>
2022-09-16 19:10:52 +0300

    updates

--- web-service-flask/predict.py
+++ web-service-flask/predict.py
@@ -6,11 +6,14 @@ from flask import Flask, request, jsonify
 with open("model_log_reg.bin", "rb") as f_in:
     model = pickle.load(f_in)
 
+def prepare_features(features):
+    X = np.array([[features['f1'], features['f2'], features['f3'],
+    features['f4'], features['f5'], features['f6'], features['f7'], features['f8'],
+    features['f9'], features['f10'], features['f11']])
+    return X
 
-def predict(features):
-    X = np.array(
-        features[["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11"]]
-    )
+
+def predict(X):
     preds = model.predict(X)
     return preds
 
--- web-service-flask/test.py
+++ web-service-flask/test.py
@@ -4,13 +4,6 @@ import numpy as np
 import json
 import predict
 
-# features = np.load("features.npy")
-
-# features = {"F": features}
-
-# features_json = json.dumps({"features": features.tolist()})
-
-# features_json = json.dumps({features.tolist()})
 
 features = {
     "f1": 2.800e02,
@@ -28,9 +21,5 @@ features = {
 
 url = "http://localhost:6000/predict"
 
-# response = requests.post(url, json=features)
 response = requests.post(url, json=features)
 print(response.json())
-
-# pred = predict.predict(features)
-# print(pred)
