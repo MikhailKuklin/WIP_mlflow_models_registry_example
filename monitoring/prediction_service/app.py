a2e30ed9f7c1774b373bfd91ae1b68bc5f879c5e
Mikhail Kuklin <kuklin1990@gmail.com>
2022-10-02 10:05:41 +0300

    remove scripts and add as lines

--- monitoring/prediction_service/app.py
+++ monitoring/prediction_service/app.py
@@ -1,27 +1,29 @@
 import os
 import pickle
 import requests
+import numpy as np
 
 from pymongo import MongoClient
 
-from flask import Flask, request, jsomify
+from flask import Flask, request, jsonify
 
 MODEL_FILE = os.getenv("MODEL_FILE", "model_log_reg.bin")
 MONGODB_ADDRESS = os.getenv("MONGODB_ADDRESS", "mongodb://127.0.0.1:27017")
-EVIDENTLY_SERVICE_ADDRESS = os.genenv("EVIDENTLY_SERVICE_", "https:///127.0.0.1.:5000")
+EVIDENTLY_SERVICE_ADDRESS = os.getenv("EVIDENTLY_SERVICE_", "https:///127.0.0.1.:5000")
 
 # open trained model
 with open(MODEL_FILE, "rb") as f_in:
     model = pickle.load(f_in)
 
 app = Flask("penguins-prediction")
-
 mongo_client = MongoClient(MONGODB_ADDRESS)
 db = mongo_client.get_database("prediction_service")
 collection = db.get_collection("data")
 
-# transform features to array formar as required by code
-def prepare_features(features):
+
+@app.route("/predict", methods=["POST"])
+def predict_endpoint():
+    features = request.get_json()
     X = np.array(
         [
             [
@@ -39,21 +41,12 @@ def prepare_features(features):
             ]
         ]
     )
-    return X
-
-
-def predict(X):
-    preds = model.predict(X)
-    return preds
-
-
-@app.route("/predict", methods=["POST"])
-def predict_endpoint():
-    features = request.get_json()
-    to_predict = prepare_features(features)
-    pred = predict(to_predict)
-    transformed_pred = transform_predict(pred)
-    result = {"penguin-sex": transformed_pred}
+    print(X)
+    y_pred = model.predict(X)
+    if y_pred[0] == 1.0:
+        result = {"penguin-sex": "male"}
+    elif y_pred[0] == 0.0:
+        result = {"penguin-sex": "female"}
 
     save_to_db(features, prediction)
     send_to_evidently_service(features, prediction)
@@ -70,7 +63,7 @@ def save_to_db(features, prediction):
 def send_to_evidently_service(features, prediction):
     rec = features.copy()
     rec["prediction"] = prediction
-    request.post(f"{EVIDENTLY_SERVICE_ADDRESS }/iterate/penguins", json=[features])
+    request.post(f"{EVIDENTLY_SERVICE_ADDRESS }/iterate/penguins", json=[rec])
 
 
 if __name__ == "__main__":
